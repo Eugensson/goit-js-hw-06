451bfcc61020068658876cb10ca08f0990f9240f
Serhii Kulbashnyi <twitch.live.ua@gmail.com>
2023-02-04 13:02:29 +0200

    6

--- js/task-10.js
+++ js/task-10.js
@@ -1,31 +1,32 @@
+const outputEl = document.querySelector("#boxes");
 const btnCreateEl = document.querySelector("[data-create]");
 const btnDestroyEl = document.querySelector("[data-destroy]");
 const quantityEl = document.querySelector('#controls>input[type="number"]');
-const outputEl = document.querySelector("#boxes");
-
-btnCreateEl.addEventListener("click", createBoxes);
-btnDestroyEl.addEventListener("click", destroyBoxes);
-
-function createBoxes(amount) {
-  const boxesArr = [];
-  for (let i = 0; i < quantityEl.value; i += 1) {
-    const boxEl = document.createElement("div");
-    boxEl.style.width = 30 + i * 10 + "px";
-    boxEl.style.height = 30 + i * 10 + "px";
-    boxEl.style.backgroundColor = getRandomHexColor();
-    boxesArr.push(boxEl);
-  }
-  outputEl.append(...boxesArr);
-  quantityEl.value = "";
-}
-
-function destroyBoxes() {
-  outputEl.innerHTML = "";
-  quantityEl.value = "";
-}
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const createBoxes = (amount) => {
+  amount = Number(quantityEl.value);
+  const boxesMarkup = new Array(amount)
+    .fill("")
+    .reduce((resultString, element, i) => {
+      return (resultString += `<div style = "width: ${30 + i * 10}px; height: ${
+        30 + i * 10
+      }px; background-color: ${getRandomHexColor()}"></div>`);
+    }, "");
+  outputEl.innerHTML = "";
+  outputEl.insertAdjacentHTML("beforeend", boxesMarkup);
+  quantityEl.value = "";
+};
+
+const removeBoxes = () => {
+  outputEl.innerHTML = "";
+  quantityEl.value = "";
+};
+
+btnCreateEl.addEventListener("click", createBoxes);
+btnDestroyEl.addEventListener("click", removeBoxes);
